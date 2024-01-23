d8d749309dfe33647bdccff4bf2a52283b50a44a
apps-code3apps <103050500+apps-code3apps@users.noreply.github.com>
2024-01-23 18:24:53 +0500

    Add Icons

--- src/navigation/AuthNavigation.js
+++ src/navigation/AuthNavigation.js
@@ -1,63 +1,85 @@
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome5';
+import routes from '../constants/routes';
+import { COLORS } from '../theme/colors';
+import { hp } from '../theme/dimensions';
 import Home from '../screens/home/Home';
 import Chat from '../screens/chat/Chat';
 import LeaderBoard from '../screens/leadBoard/LeaderBoard';
-import Canvass from '../screens/canvass/Canvass';
 import PhoneBankingStack from './PhoneBankingStack';
-import {getFocusedRouteNameFromRoute} from '@react-navigation/native';
-import routes from '../constants/routes';
+import Canvass from '../screens/canvass/Canvass';
 
 const Tab = createBottomTabNavigator();
 
+const screenOptions = ({ route }) => ({
+  headerShown: false,
+  tabBarStyle: { display: getTabBarVisibility(route) },
+  tabBarActiveTintColor: COLORS.orangeReddish,
+  tabBarInactiveTintColor: COLORS.lavendarWhiteDark,
+  tabBarIcon: ({ focused, color, size }) => {
+    let iconName;
+
+    switch (route.name) {
+      case routes.Home:
+        iconName = 'home';
+        break;
+      case routes.Chat:
+        iconName = 'comments';
+        break;
+      case routes.LeaderBoard:
+        iconName = 'chart-bar';
+        break;
+      case routes.PhoneBankingStack:
+        iconName = 'phone';
+        break;
+      case routes.Canvass:
+        iconName = 'map-marker-alt';
+        break;
+      default:
+        iconName = ''; // Handle default case
+    }
+
+    return <Icon name={iconName} size={hp(2.8)} color={focused ? COLORS.orangeReddish : COLORS.lavendarWhiteDark} />;
+  },
+});
+
 const AuthNavigation = () => {
   return (
-    <Tab.Navigator
-      screenOptions={({route}) => ({
-        headerShown: false,
-        tabBarStyle: {display: getTabBarVisibility(route)},
-      })}>
+    <Tab.Navigator screenOptions={screenOptions}>
       <Tab.Screen
         name={routes.Home}
         component={Home}
-        options={{tabBarLabel: routes.Home}}
+        options={{ tabBarLabel: routes.Home }}
       />
       <Tab.Screen
         name={routes.Chat}
         component={Chat}
-        options={{tabBarLabel: routes.Chat}}
+        options={{ tabBarLabel: routes.Chat }}
       />
       <Tab.Screen
         name={routes.LeaderBoard}
         component={LeaderBoard}
-        options={{tabBarLabel: routes.LeaderBoard}}
+        options={{ tabBarLabel: routes.LeaderBoard }}
       />
       <Tab.Screen
         name={routes.PhoneBankingStack}
         component={PhoneBankingStack}
-        options={{tabBarLabel: routes.PhoneBank}}
+        options={{ tabBarLabel: routes.PhoneBank }}
       />
       <Tab.Screen
         name={routes.Canvass}
         component={Canvass}
-        options={{tabBarLabel: routes.Canvass}}
+        options={{ tabBarLabel: routes.Canvass }}
       />
     </Tab.Navigator>
   );
 };
 
-function getTabBarVisibility(route) {
+const getTabBarVisibility = (route) => {
   const routeName = getFocusedRouteNameFromRoute(route);
-
-  if (
-    [routes.VoterCheck, routes.TotalVoters, routes.AddToTeam].includes(
-      routeName,
-    )
-  ) {
-    return 'none';
-  }
-
-  return 'flex';
-}
+  return [routes.VoterCheck, routes.TotalVoters, routes.AddToTeam].includes(routeName) ? 'none' : 'flex';
+};
 
 export default AuthNavigation;
